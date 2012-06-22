e1b74ef6b89c178d2813e500bdb722658a2f2c7e
Jonathan Smiley <jonathan@zurb.com>
2012-06-21 17:39:28 -0700

    Added some of the buttons docs

--- marketing/docs/buttons.php
+++ marketing/docs/buttons.php
@@ -6,15 +6,45 @@
 		  <div class="row">
 		    <div class="eight columns">
     			<h3>Buttons</h3>
-    			<h4 class="subheader">Subheader</h4>
-
-    			<h4>Intro</h4>
-  				<p>Intro text.</p>
+    			<h4 class="subheader">Buttons may be a hack (thanks Josh Clark) but they're an awfully convenient hack when it comes to more traditional actions. To that end Foundation includes a lot of easy to use button styles that you can customize or override.</h4>
+    			
+  				<p>Foundation buttons have a number of parameters and styles - you can see a few examples below. The out of the box classes include size, color and style (square, slightly rounded, and completely rounded).</p>
+  				
+  				<script src="https://gist.github.com/2969496.js?file=f3-buttons.html"></script>
+  				
+  				<div class="row">
+  					<div class="six columns">
+  						<a href="#" class="tiny button">Tiny Button &raquo;</a><br /><br />
+  						<a href="#" class="small button">Small Button &raquo;</a><br /><br />
+  						<a href="#" class="button">Regular Button &raquo;</a><br /><br />
+  						<a href="#" class="large button">Large Button &raquo;</a><br /><br />
+  						<br /><br />
+  						<a href="#" class="tiny secondary button">Secondary Button &raquo;</a><br /><br />
+  						<a href="#" class="small secondary button">Secondary Button &raquo;</a><br /><br />
+  						<a href="#" class="secondary button">Secondary Button &raquo;</a><br /><br />
+  						<a href="#" class="large secondary button">Secondary Button &raquo;</a><br /><br />
+  						<br /><br />
+  					</div>
+  					<div class="six columns">
+  						<a href="#" class="tiny success button">Success Button &raquo;</a><br /><br />
+  						<a href="#" class="small success button">Success Button &raquo;</a><br /><br />
+  						<a href="#" class="success button">Success Button &raquo;</a><br /><br />
+  						<a href="#" class="large success button">Success Button &raquo;</a><br /><br />
+  						<br /><br />
+  						<a href="#" class="tiny alert button">Alert Button &raquo;</a><br /><br />
+  						<a href="#" class="small alert button">Alert Button &raquo;</a><br /><br />
+  						<a href="#" class="alert button">Alert Button &raquo;</a><br /><br />
+  						<a href="#" class="large alert button">Alert Button &raquo;</a><br /><br />
+  						<br /><br />
+  					</div>
+  				</div>
+  				
+  				<p>Button classes can also be applied to <code>button</code> elements, as well as <code>input type="submit"</code> elements.</p>
 
   				<hr />
 
-  				<h4>Section</h4>
-  				<p>Description.</p>
+  				<h4>Button Groups</h4>
+  				<p>When you need a group of actions, button groups give you some easy to use options.</p>
 
     		</div>
     		<div class="four columns">
